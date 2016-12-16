755138dd1714273940161309bc74464756db7b1f
Kristian Slosar <slosar.kristian@gmail.com>
2016-12-16 20:10:33 +0100

    Added a project route and updated menu/header

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/about', (req,res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "Portfolio page"
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
